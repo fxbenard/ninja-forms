e48a8bb8d54117ca1cf061318707f8e164eb64a5
James <james@wpninjas.net>
2013-05-25 00:05:52 -0400

    Added a form class filter and general clean-up
    added the filter ninja_forms_form_class

--- includes/display/form/open-form-tag.php
+++ includes/display/form/open-form-tag.php
@@ -1,21 +1,24 @@
 <?php
-add_action('init', 'ninja_forms_register_display_open_form_tag');
-function ninja_forms_register_display_open_form_tag(){
-	add_action('ninja_forms_display_open_form_tag', 'ninja_forms_display_open_form_tag');
-}
 
-function ninja_forms_display_open_form_tag($form_id){
-	$form_row = ninja_forms_get_form_by_id($form_id);
-	if( isset( $form_row['data']['ajax'] ) ){
+function ninja_forms_register_display_open_form_tag() {
+	add_action( 'ninja_forms_display_open_form_tag', 'ninja_forms_display_open_form_tag' );
+}
+add_action( 'init', 'ninja_forms_register_display_open_form_tag' );
+
+function ninja_forms_display_open_form_tag( $form_id ) {
+
+	$form_row = ninja_forms_get_form_by_id( $form_id );
+
+	if ( isset ( $form_row['data']['ajax'] ) )
 		$ajax = $form_row['data']['ajax'];
-	}else{
+	else
 		$ajax = 0;
-	}
-	if($ajax == 1){
+
+	if ( $ajax == 1 ) {
 		$url = admin_url( 'admin-ajax.php' );
-		$url = add_query_arg('action', 'ninja_forms_ajax_submit', $url);
+		$url = add_query_arg( 'action', 'ninja_forms_ajax_submit', $url );
 		//$url = add_query_arg('action', 'test', $url);
-	}else{
+	} else {
 		$url = '';
 	}
 
@@ -23,13 +26,20 @@ function ninja_forms_display_open_form_tag($form_id){
 
 	$display = apply_filters( 'ninja_forms_display_form_visibility', $display, $form_id );
 
-	if( $display != 1 ){
+	if ( $display != 1 )
 		$hide_class = " ninja-forms-no-display";
-	} else {
+	else
 		$hide_class = "";
-	}
+
+	$form_class = '';
+
+	$form_class = apply_filters( 'ninja_forms_form_class', $form_class, $form_id );
+
+	if ( ! empty( $form_class ) )
+		$form_class = ' ' . $form_class;
+
 	?>
-	<form id="ninja_forms_form_<?php echo $form_id;?>" enctype="multipart/form-data" method="post" name="" action="<?php echo $url;?>" class="ninja-forms-form<?php echo $hide_class;?>">
-	
+	<form id="ninja_forms_form_<?php echo $form_id;?>" enctype="multipart/form-data" method="post" name="" action="<?php echo $url;?>" class="ninja-forms-form<?php echo $form_class;?><?php echo $hide_class;?>">
+
 	<?php
 }
