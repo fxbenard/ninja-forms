6b33d862c80081ecee12ec82cf3c24efe182389b
Kevin Stover <kstover@gmail.com>
2013-06-07 11:11:18 +0100

    Bugfix for restoring user progress
    There was a bug in how the user's progress was being replaced in the
    form.

--- includes/display/fields/restore-progress.php
+++ includes/display/fields/restore-progress.php
@@ -10,25 +10,18 @@ function ninja_forms_register_filter_restore_progress(){
 }
 
 function ninja_forms_filter_restore_progress( $data, $field_id ){
-	global $current_user, $ninja_forms_processing, $ninja_forms_fields;
+	global $ninja_forms_processing, $ninja_forms_fields;
 
 	$field_row = ninja_forms_get_field_by_id( $field_id );
 	$field_type = $field_row['type'];
 
-	$process_field = $ninja_forms_fields[$field_type]['process_field'];
-
-	get_currentuserinfo();
-	$form_row = ninja_forms_get_form_by_field_id( $field_id );
-	$form_id = $form_row['id'];
-	
-	if( isset( $current_user ) ){
-		$user_id = $current_user->ID;	
+	if( isset( $ninja_forms_fields[$field_type]['process_field'] ) ){
+		$process_field = $ninja_forms_fields[$field_type]['process_field'];
 	}else{
-		$user_id = '';
+		$process_field = false;
 	}
-		
-	if( is_object( $ninja_forms_processing ) AND $process_field ){
 
+	if( is_object( $ninja_forms_processing ) AND $process_field ){
 		$clear_form = $ninja_forms_processing->get_form_setting( 'clear_complete' );
 		$process_complete = $ninja_forms_processing->get_form_setting( 'processing_complete' );
 		if( $process_complete != 1 OR ( $process_complete == 1 AND $clear_form != 1 ) ){
