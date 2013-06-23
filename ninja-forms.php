aaaccb332915cad51ba90dff3a8e74af1155619a
Kevin Stover <kstover@gmail.com>
2013-06-23 23:47:01 +0100

    Added a new field-wrap div to the form display output.

--- ninja-forms.php
+++ ninja-forms.php
@@ -126,6 +126,7 @@ require_once( NINJA_FORMS_DIR . "/includes/display/form/form-title.php" );
 require_once( NINJA_FORMS_DIR . "/includes/display/form/process-message.php" );
 require_once( NINJA_FORMS_DIR . "/includes/display/fields/field-error-message.php" );
 require_once( NINJA_FORMS_DIR . "/includes/display/form/form-wrap.php" );
+require_once( NINJA_FORMS_DIR . "/includes/display/form/fields-wrap.php" );
 require_once( NINJA_FORMS_DIR . "/includes/display/form/required-label.php" );
 require_once( NINJA_FORMS_DIR . "/includes/display/form/open-form-tag.php" );
 require_once( NINJA_FORMS_DIR . "/includes/display/form/close-form-tag.php" );
