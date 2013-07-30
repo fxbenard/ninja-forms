44431744a270c0d7ab8cdd8db868bb55677e3719
Kevin Stover <kstover@gmail.com>
2013-07-30 13:35:11 -0400

    Changed help.php so that it gets $data passed to it rather than getting the field settings $data from the database. This makes it possible to modify the help_text and show_help using the ninja_forms_field filter.

--- includes/display/fields/help.php
+++ includes/display/fields/help.php
@@ -3,16 +3,9 @@
  * Outputs the HTML of the help icon if it is set to display.
  *
 **/
-add_action('init', 'ninja_forms_register_display_field_help');
-function ninja_forms_register_display_field_help(){
-	add_action( 'ninja_forms_display_field_help', 'ninja_forms_display_field_help' );
-}
 
-function ninja_forms_display_field_help( $field_id ){
+function ninja_forms_display_field_help( $field_id, $data ){
 	$plugin_settings = get_option( 'ninja_forms_settings' );
-
-	$field_row = ninja_forms_get_field_by_id( $field_id );
-	$data = $field_row['data'];
 
 	if( isset( $data['show_help'] ) ){
 		$show_help = $data['show_help'];
@@ -26,11 +19,11 @@ function ninja_forms_display_field_help( $field_id ){
 		$help_text = '';
 	}
 
-
-
 	if($show_help){
 		?>
 		<img id="" class='ninja-forms-help-text' src="<?php echo NINJA_FORMS_URL;?>/images/question-ico.gif" title="<?php echo $help_text;?>">
 	<?php
 	}
-}
+}	
+
+add_action( 'ninja_forms_display_field_help', 'ninja_forms_display_field_help', 10, 2 );
